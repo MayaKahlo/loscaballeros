c9674450374207a1c5d6a0c57d2249de7f04d12f
Larubbio <karinalarubbio@gmail.com>
2026-04-28 15:43:23 -0400

    connecting backend

---
+++ process_image.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+from flask_cors import CORS 
+
+app = Flask(__name__)
+CORS(app) 
+
+@app.route('/api/data')
+def get_data():
+    return jsonify({"message": "Hello from Python!"})

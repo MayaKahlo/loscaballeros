cec8f460149c60b34d64facfb125e072bce278f4
Larubbio <karinalarubbio@gmail.com>
2026-05-08 10:25:49 -0400

    more cleanup and demo

---
+++ 3d-playtime/unity/Assets/GeneratePlanes.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class PlaneTransform
+{
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 scale;
+}
+
+[System.Serializable]
+public class PlaneTransformList{
+    public PlaneTransform[] list;
+
+}
+
+public class GeneratePlanes : MonoBehaviour
+{
+    string filepath = "Assets/plane_transforms.json";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Read in json
+        string jsonString = System.IO.File.ReadAllText(filepath);
+        jsonString = "{\"list\":" + jsonString + "}";
+        PlaneTransformList planeTransformList = JsonUtility.FromJson<PlaneTransformList>(jsonString);
+
+        Debug.Log("Number of planes: " + planeTransformList.list.Length);
+
+
+        foreach (var planeTransform in planeTransformList.list)
+        {
+            GameObject plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            plane.transform.position = new Vector3(planeTransform.position.x, planeTransform.position.z, planeTransform.position.y);
+            plane.transform.rotation = Quaternion.Euler(planeTransform.rotation.x, planeTransform.rotation.z, planeTransform.rotation.y);
+            plane.transform.localScale = new Vector3(planeTransform.scale.x, planeTransform.scale.z, planeTransform.scale.y);
+        }
+
+        Debug.Log("all planes loaded successfully");
+    }
+
+}
